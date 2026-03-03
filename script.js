ba864c3a3910de99f00a43ce821b2c67d499d315
Roger <rogerplaballus@gmail.com>
2026-03-03 13:42:54 +0100

    Initial commit - weather app with API integration

--- script.js
+++ script.js
@@ -53,5 +53,11 @@ searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 })
 
+searchBox.addEventListener("keypress", (event)=>{
+    if(event.key === "Enter"){
+        checkWeather(searchBox.value);
+    }
+})
+
 
 
